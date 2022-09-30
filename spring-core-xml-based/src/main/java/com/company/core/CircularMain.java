2040f189a6493f1037548a6988f4a51744440af1
talibnabi <talibnabiyev@gmail.com>
2022-09-30 04:14:10 -0700

    commit

--- spring-core-xml-based/src/main/java/com/company/core/CircularMain.java
+++ spring-core-xml-based/src/main/java/com/company/core/CircularMain.java
@@ -7,6 +7,6 @@ public class CircularMain {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-circular.xml");
         Department department = context.getBean("dept", Department.class);
-        
+
     }
 }
