f356355f5a26b45fbfb51665bde5f9b4d804e606
talibnabi <talibnabiyev@gmail.com>
2022-10-01 02:11:42 -0700

    commit

--- spring-core-xml-based/src/main/java/com/company/core/CollectionHolder.java
+++ spring-core-xml-based/src/main/java/com/company/core/CollectionHolder.java
@@ -7,10 +7,19 @@ import java.util.Set;
 public class CollectionHolder {
     private List<String> myList;
     private Set<String> mySet;
-    private Map<String,String> myMap;
+    private Map<String, String> myMap;
+    private List<Player> players;
 
     public List<String> getMyList() {
         return myList;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
     }
 
     public void setMyList(List<String> myList) {
--- spring-core-xml-based/src/main/java/com/company/core/CollectionHolderMain.java
+++ spring-core-xml-based/src/main/java/com/company/core/CollectionHolderMain.java
@@ -9,11 +9,12 @@ public class CollectionHolderMain {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-collection.xml");
         CollectionHolder holder = context.getBean("myCollection", CollectionHolder.class);
-        List<String> myList = holder.getMyList();
-        System.out.println(holder.getMyList());
-        myList.set(0,"Alisa");
+//        List<String> myList = holder.getMyList();
+//        System.out.println(holder.getMyList());
+//        myList.set(0,"Alisa");
         System.out.println(holder.getMyList());
         System.out.println(holder.getMySet());
         System.out.println(holder.getMyMap());
+        System.out.println(holder.getPlayers());
     }
 }
