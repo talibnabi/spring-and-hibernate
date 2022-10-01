2adf09cd100b256e8978d9246b45683482c69584
talibnabi <talibnabiyev@gmail.com>
2022-10-01 03:21:59 -0700

    commit

---
+++ spring-core-xml-based/src/main/java/com/company/core/Album.java
@@ -0,0 +1,20 @@
+package com.company.core;
+
+public class Album {
+    private String albumName;
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "albumName='" + albumName + '\'' +
+                '}';
+    }
+}
---
+++ spring-core-xml-based/src/main/java/com/company/core/Song.java
@@ -0,0 +1,30 @@
+package com.company.core;
+
+public class Song {
+    private String songName;
+    private String song;
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "songName='" + songName + '\'' +
+                ", song='" + song + '\'' +
+                '}';
+    }
+}
