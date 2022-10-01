9a431fb3581bf62bb8653f7cd5fd6a80b1da2d6f
talibnabi <talibnabiyev@gmail.com>
2022-10-01 13:14:35 -0700

    commit

--- spring-core-xml-based/src/main/java/com/company/core/myNote/innerBean/Main.java
+++ spring-core-xml-based/src/main/java/com/company/core/myNote/innerBean/Main.java
@@ -5,6 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("myNote/app-context-inner-bean.xml");
+        Person person = context.getBean(Person.class);
+        Person person1 = context.getBean(Person.class);
+        System.out.println(person.hashCode());
+        System.out.println(person1.hashCode());
     }
 }
