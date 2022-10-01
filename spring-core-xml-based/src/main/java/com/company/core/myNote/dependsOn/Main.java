d0076eacfb636d4525abd18aa4e300ce4b2ed765
talibnabi <talibnabiyev@gmail.com>
2022-10-01 05:06:32 -0700

    commit

--- spring-core-xml-based/src/main/java/com/company/core/myNote/dependsOn/Main.java
+++ spring-core-xml-based/src/main/java/com/company/core/myNote/dependsOn/Main.java
@@ -1,7 +1,13 @@
 package com.company.core.myNote.dependsOn;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-
+        ApplicationContext context=new ClassPathXmlApplicationContext("myNote/app-context-depends-on.xml");
+        context.getBean(Aitaj.class);
+        context.getBean(Lamiya.class);
+        context.getBean(Alisa.class);
     }
 }
