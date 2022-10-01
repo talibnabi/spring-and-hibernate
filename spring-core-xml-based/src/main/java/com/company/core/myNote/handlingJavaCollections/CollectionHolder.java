05747a6a415f32c371d700bdc53137f299b150c5
talibnabi <talibnabiyev@gmail.com>
2022-10-01 05:55:43 -0700

    commit

--- spring-core-xml-based/src/main/java/com/company/core/myNote/handlingJavaCollections/CollectionHolder.java
+++ spring-core-xml-based/src/main/java/com/company/core/myNote/handlingJavaCollections/CollectionHolder.java
@@ -12,4 +12,43 @@ public class CollectionHolder {
     private Properties properties;
     private List<Soldier> mySoldier;
 
+    public List<String> getMyList() {
+        return myList;
+    }
+
+    public void setMyList(List<String> myList) {
+        this.myList = myList;
+    }
+
+    public Set<String> getMySet() {
+        return mySet;
+    }
+
+    public void setMySet(Set<String> mySet) {
+        this.mySet = mySet;
+    }
+
+    public Map<String, String> getMyMap() {
+        return myMap;
+    }
+
+    public void setMyMap(Map<String, String> myMap) {
+        this.myMap = myMap;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public List<Soldier> getMySoldier() {
+        return mySoldier;
+    }
+
+    public void setMySoldier(List<Soldier> mySoldier) {
+        this.mySoldier = mySoldier;
+    }
 }
