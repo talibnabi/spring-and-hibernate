6c932474b04ee5403060e9d999ac6128aa253521
talibnabi <talibnabiyev@gmail.com>
2022-12-11 10:13:29 -0800

    commit

--- springdi2/src/main/java/com/company/springdi2/CricketCoach.java
+++ springdi2/src/main/java/com/company/springdi2/CricketCoach.java
@@ -3,12 +3,31 @@ package com.company.springdi2;
 public class CricketCoach implements Coach {
     private FortuneService fortuneService;
 
+    private String emailAddress;
+    private String team;
+
     public CricketCoach() {
         System.out.println("CRICKET COACH");
     }
 
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
     }
 
     @Override
--- springdi2/src/main/java/com/company/springdi2/MyApp.java
+++ springdi2/src/main/java/com/company/springdi2/MyApp.java
@@ -7,9 +7,14 @@ public class MyApp {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Coach cricketCoach = context.getBean("myCoach", Coach.class);
+        CricketCoach cricketCoach1 = context.getBean("myCoach", CricketCoach.class);
         String fortune = cricketCoach.getDailyFortune();
         String workout = cricketCoach.getDailyWorkout();
         System.out.println(fortune);
         System.out.println(workout);
+//        cricketCoach1.setEmailAddress("talibnabiyev@gmail.com");
+//        cricketCoach1.setTeam("NRDTLN");
+        System.out.println("EMAIL: " + cricketCoach1.getEmailAddress());
+        System.out.println("TEAM: " + cricketCoach1.getTeam());
     }
 }
